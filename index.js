b797da864d995f7dc392d4c5cbf5a846c53defda
Boi <cambodianjesuss@gmail.com>
2023-10-29 13:55:33 -0700

    emmiting event and sending to event bus

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -17,7 +18,7 @@ app.get('/posts', (req, res)=>{
     res.send(posts);
 });
 
-app.post('/posts', (req, res)=>{
+app.post('/posts', async (req, res)=>{
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
 
@@ -25,6 +26,14 @@ app.post('/posts', (req, res)=>{
         id,
         title
     }
+
+    // Events to emmit
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, title
+        }
+    });
 
     res.status(201).send(posts[id]);
 });
